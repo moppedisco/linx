4eb276491a50d1be356cf3f61fdb0ebfb448a866
Yacine Belarbi <yacbel@gmail.com>
2017-03-17 10:32:07 +0100

    added google tag manager

--- wp-content/themes/linx/header.php
+++ wp-content/themes/linx/header.php
@@ -13,6 +13,14 @@
 
 	<title><?php wp_title(''); ?></title>
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-XXXX');</script>
+	<!-- End Google Tag Manager -->
+
 	<?php // mobile meta (hooray!) ?>
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
@@ -40,6 +48,10 @@
 </head>
 
 <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="container">
 	<nav class='menu-main-nav' role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 		<?php wp_nav_menu(array(
